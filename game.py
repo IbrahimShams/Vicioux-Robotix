4d71f7ba9c78a8864a637334d6f3bbccf82d6242
IbrahimShams <yppolppolp@gmail.com>
2023-06-02 12:14:40 -0400

    June 2nd, Day 2 - edit

--- game.py
+++ game.py
@@ -18,6 +18,7 @@ from math import *
 
 width,height=1150,697
 screen=display.set_mode((width,height))
+display.set_caption("Vicious Robotix")
 RED=(255,0,0)
 GREY=(127,127,127)
 BLACK=(0,0,0)
@@ -33,53 +34,64 @@ rect_list=[Rect(0,450,50,50)]
 omx,omy=0,0
 lavaImgs=[image.load("lava/lava00"+f"{i}"+".png").convert() for i in range(6)]
 lava_background=Rect(0,667,1150,30)
-# gravity=3
-# jumpPower=-45
+player=image.load("player/tile000.png")
+player=transform.scale(player,(100,100))
 
-# X,Y,W,H=0,1,2,3
+gravity=2
+jumpPower=-25
+doubleJump=False
 
-# def drawScene():
-#     screen.fill(WHITE)
-#     draw.rect(screen,RED,p)
-#     for plat in rect_list:
-#         screen.blit(block,plat)
-#     display.flip()
+X,Y,W,H=0,1,2,3
 
-# def movePlayer(p):
-#     keys=key.get_pressed()
-#     if keys[K_SPACE] and p[Y]+p[H]==v[2] and v[Y]==0:
-#         v[Y]=jumpPower
+def drawScene():
+    global lava_counter
+    screen.fill(WHITE)
+    draw.rect(screen,RED,p)
+    for plat in rect_list:
+        screen.blit(block,plat)
+    draw.rect(screen,(220,70,40),lava_background)
+    lava_counter=animate(lavaImgs,lava_counter,0.1,0,580)
+    screen.blit(player,(0,0))
+    display.flip()
 
-#     v[X]=0
-#     if keys[K_LEFT]:
-#         v[X]=-10
-#     elif keys[K_RIGHT]:
-#         v[X]=10
+def movePlayer(p):
+    keys=key.get_pressed()
+    if keys[K_SPACE] and p[Y]+p[H]==v[2] and v[Y]==0:
+        v[Y]=jumpPower
+        doubleJump=True
+    
+    elif doubleJump:
+        v[Y]=jumpPower
+        doubleJump=False
+
+    v[X]=0
+    if keys[K_LEFT]:
+        v[X]=-7
+    elif keys[K_RIGHT]:
+        v[X]=7
   
-#     p[X]+=v[X] #horizontal movement
-#     v[Y]+=gravity
+    p[X]+=v[X]
+    if v[Y]<=25:
+        v[Y]+=gravity
 
-# def check(p):
-#     #we will first check if the player lands on
-#     #one of the platforms
-#     for plat in rect_list:
-#                                                         #   current position         next frame position
-#         if p[X]+p[W]>plat[X] and p[X]<plat[X]+plat[W] and p[Y]+p[H]<=plat[Y] and p[Y]+p[H]+v[Y]>=plat[Y]:
-#             v[Y]=0#stop falling down
-#             v[2]=plat[Y]
-#             p[Y]=plat[Y]-p[H]
+def check(p):
+    for plat in rect_list:                                            #   current position         next frame position
+        if p[X]+p[W]>plat[X] and p[X]<plat[X]+plat[W] and p[Y]+p[H]<=plat[Y] and p[Y]+p[H]+v[Y]>=plat[Y]:
+            v[Y]=0
+            v[2]=plat[Y]
+            p[Y]=plat[Y]-p[H]
 
-#     p[Y]+=v[Y]#vertical movement
+    p[Y]+=v[Y]#vertical movement
 
-#     # if p[Y]+p[H]>=600:
-#     #     v[Y]=0
-#     #     p[Y]=600-p[H]
-#     #     v[2]=600
+    # if p[Y]+p[H]>=600:
+    #     v[Y]=0
+    #     p[Y]=600-p[H]
+    #     v[2]=600
 
-# #hor ver height
-# v=[0,0, 600]
+#hor ver
+v=[0,0,697]
 
-# p=Rect(0,250,50,50)
+p=Rect(0,250,50,50)
 
 for img in lavaImgs:
     img.set_colorkey(BLACK)
@@ -141,7 +153,6 @@ def animate(lst,counter,speed,x,y):
     return counter
 
 while running:
-    screen.fill(WHITE)
     for evt in event.get():
         if evt.type==QUIT:
             running=False
@@ -154,14 +165,10 @@ while running:
     elif mb[2]:
         eraseMap(roundIt(omx,50),roundIt(omy,50),roundIt(mx,50),roundIt(my,50),rect_list)
     
-    # drawScene()
-    draw.rect(screen,(220,70,40),lava_background)
-    lava_counter=animate(lavaImgs,lava_counter,0.1,0,580)
-    for rect in rect_list:
-         screen.blit(block,rect)
-    # movePlayer(p)
-    # check(p)
-    #print(len(rect_list))
+    drawScene()
+    movePlayer(p)
+    check(p)
+    print(len(rect_list))
     myClock.tick(60)
     display.flip()
     omx,omy=mx,my
