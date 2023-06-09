09d50715b91bc730b79384ea91cbc5854d43ae6e
IbrahimShams <yppolppolp@gmail.com>
2023-06-09 16:02:53 -0400

    Polished Version

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
 from pygame import *
 from math import *
+from random import *
 
 width,height=1150,697
 screen=display.set_mode((width,height))
-display.set_caption("Vicious Robotix")
+display.set_caption("Vicioux Robotix")
 RED=(255,0,0)
-GREY=(127,127,127)
+GREY=(200,200,200)
 BLACK=(0,0,0)
 BLUE=(0,0,255)
 GREEN=(0,255,0)
@@ -14,21 +15,22 @@ WHITE=(255,255,255)
 myClock=time.Clock()
 running=True
 lava_counter=0
-block=image.load("block1.png")
-rect_list=[Rect(0,450,50,50)]
+block=image.load("block2.png").convert()
+rect_list=[]
 omx,omy=0,0
 lavaImgs=[image.load("lava/lava00"+f"{i}"+".png").convert() for i in range(6)]
 lava_background=Rect(0,597,1150,100)
-
+nospawnregion=Rect(0,275,250,150)
+robot_platforms=[Rect(randint(0,975),randint(0,405),175,20)]
 jumpPower=-7
 move_list=["no jump","right",0.07]
-dJump,facing,img_speed=0,1,2,
+dJump,facing,img_speed=0,1,2
 gravity=0.3
 X,Y,W,H=0,1,2,3
+timer,moved=0,False
 
-#hor ver
 v=[0,0,697]
-p=Rect(0,375,75,75)
+p=Rect(0,400,75,75)
 p_list=[0,0]
 objects=[lava_background,p]
 
@@ -41,7 +43,7 @@ def flipPics(lst):
 def addPics(name,start,end):
     mypics=[]
     for i in range(start,end):
-        img=image.load(f"player/{name}{i:03}.png")
+        img=image.load(f"player/{name}{i:03}.png").convert_alpha()
         img=transform.scale(img,(75,75))
         mypics.append(img)
     return mypics
@@ -57,14 +59,17 @@ pics.append(addPics("tile",64,71)) #attack right
 pics.append(flipPics(pics[6])) #attack left
 
 def drawScene():
-    screen.fill(BLUE)
+    screen.fill(GREY)
+    draw.rect(screen,RED,nospawnregion,1)
     draw.rect(screen,RED,p,1)
+    for plat in robot_platforms:
+        draw.rect(screen,RED,plat,1)
+    for plat in rect_list:
+        screen.blit(block,plat)
     row=p_list[0]
     col=int(p_list[1])
     pic=pics[row][col]
     screen.blit(pic,p)
-    for plat in rect_list:
-        screen.blit(block,plat)
 
 def hitWalls(x,y,walls):
     playerRect=Rect(x,y,75,75)
@@ -84,29 +89,24 @@ def movePlayer(p,move_list):
         if v[Y]==0:
             p_list[0]=3
             move_list[img_speed]=0.15
-
     if keys[K_d] and hitWalls(p[X]+5,p[Y],rect_list)==-1:
         v[X]=5
         move_list[facing]="right"
         if v[Y]==0:
             p_list[0]=2
             move_list[img_speed]=0.15
-
     if keys[K_w] and p[Y]+p[H]==v[2] and v[Y]==0:
         v[Y]=jumpPower
         move_list[dJump]="first jump"
-
     if not keys[K_w] and move_list[dJump]=="first jump":
         move_list[dJump]="double jump available"
     if move_list[dJump]=="double jump available" and keys[K_w]:
         v[Y]=jumpPower
         move_list[dJump]="no jump"
-    if v[Y]==jumpPower:
-        p_list[1]=0
     if v[Y]<0:
+        move_list[img_speed]=0.33
         if hitWalls(p[X],p[Y]+v[Y],rect_list)!=-1:
             v[Y]=-gravity
-        move_list[img_speed]=0.33
         if move_list[facing]=="right":
             p_list[0]=4
         else:
@@ -117,6 +117,7 @@ def movePlayer(p,move_list):
             p_list[0]=6
         else:
             p_list[0]=7
+
     p_list[1]=(p_list[1]+move_list[img_speed])%len(pics[p_list[0]])
 
     p[X]+=v[X]
@@ -124,18 +125,18 @@ def movePlayer(p,move_list):
         v[Y]+=gravity
 
 def check(p):
-    for plat in rect_list:                                            #   current position         next frame position
+    global timer
+    for plat in rect_list:
         if p[X]+p[W]>plat[X] and p[X]<plat[X]+plat[W] and p[Y]+p[H]<=plat[Y] and p[Y]+p[H]+v[Y]>=plat[Y]:
             v[Y]=0
             v[2]=plat[Y]
             p[Y]=plat[Y]-p[H]
-
-    p[Y]+=int(v[Y])#vertical movement
-
-    # if p[Y]+p[H]>=600:
-    #     v[Y]=0
-    #     p[Y]=600-p[H]
-    #     v[2]=600
+    if not moved:
+        if timer<180:
+            timer+=1
+            v[Y]=0
+    else:
+        p[Y]+=int(v[Y])
 
 for img in lavaImgs:
     img.set_colorkey(BLACK)
@@ -204,10 +205,16 @@ def animate(lst,counter,speed,x,y):
     return (counter+speed)%len(lst)
 
 while running:
+    if nospawnregion.colliderect(robot_platforms[0]):
+        robot_platforms[0]=Rect(randint(0,975),randint(0,405),175,20)
     for evt in event.get():
         if evt.type==QUIT:
             running=False
-
+        if evt.type==KEYDOWN:
+            if evt.key==K_w or evt.key==K_LSHIFT:
+                p_list[1]=0
+            if evt.key==K_w or evt.key==K_a or evt.key==K_d:
+                moved=True
     mx,my=mouse.get_pos()
     mb=mouse.get_pressed()
     
@@ -222,6 +229,7 @@ while running:
     lava_counter=animate(lavaImgs,lava_counter,0.1,0,580)
     movePlayer(p,move_list)
     check(p)
+    print(len(rect_list))
     myClock.tick(60)
     display.update()
     omx,omy=mx,my
