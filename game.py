0e188d58ac15e899ac978c8b175b5201d5325f1a
IbrahimShams <yppolppolp@gmail.com>
2023-06-12 13:22:07 -0400

    June 12th, 2023

--- game.py
+++ game.py
@@ -20,55 +20,99 @@ rect_list=[]
 omx,omy=0,0
 lavaImgs=[image.load("lava/lava00"+f"{i}"+".png").convert() for i in range(6)]
 lava_background=Rect(0,597,1150,100)
-nospawnregion=Rect(0,275,250,150)
-robot_platforms=[Rect(randint(0,975),randint(0,405),175,20)]
+no_spawn_region=Rect(0,275,250,150)
+robot_hitboxes=[]
 jumpPower=-7
 move_list=["no jump","right",0.07]
 dJump,facing,img_speed=0,1,2
 gravity=0.3
 X,Y,W,H=0,1,2,3
-timer,moved=0,False
+moved=False
+begin=time.get_ticks()
+start_timer=time.get_ticks()
+SPEED=7
 
 v=[0,0,697]
 p=Rect(0,400,75,75)
 p_list=[0,0]
 objects=[lava_background,p]
+bullets=[]
 
 def flipPics(lst):
-    flip_lst=[]
-    for image in lst:
-        flip_lst.append(transform.flip(image,True,False))
-    return flip_lst
+    return [transform.flip(image,True,False) for image in lst]
 
-def addPics(name,start,end):
+def addPics(source,name,start,end):
     mypics=[]
-    for i in range(start,end):
-        img=image.load(f"player/{name}{i:03}.png").convert_alpha()
+    for i in range(start,end+1):
+        img=image.load(f"{source}/{name}{i:03}.png").convert_alpha()
         img=transform.scale(img,(75,75))
         mypics.append(img)
     return mypics
 
-pics=[]
-pics.append(addPics("tile",0,5)) #idle right
-pics.append(flipPics(pics[0])) #idle left
-pics.append(addPics("tile",24,31)) #walking right
-pics.append(flipPics(pics[2])) #walking left
-pics.append(addPics("tile",40,47)) #jumping right
-pics.append(flipPics(pics[4])) #jumping left
-pics.append(addPics("tile",64,71)) #attack right
-pics.append(flipPics(pics[6])) #attack left
+player_pics=[]
+player_pics.append(addPics("player","tile",0,4)) #idle right
+player_pics.append(flipPics(player_pics[0])) #idle left
+player_pics.append(addPics("player","tile",24,31)) #walking right
+player_pics.append(flipPics(player_pics[2])) #walking left
+player_pics.append(addPics("player","tile",40,47)) #jumping right
+player_pics.append(flipPics(player_pics[4])) #jumping left
+player_pics.append(addPics("player","tile",64,71)) #attack right
+player_pics.append(flipPics(player_pics[6])) #attack left
+
+robot_pics=[]
+robot_pics.append(addPics("robots","tile",0,7))
+
+def roundIt(num,round_num):
+    n=0
+    for i in range(0,num+1,round_num):
+        if i+round_num>num:
+            n=i
+    return n
+
+def generatePlatform():
+    while True:
+        robotHitbox=Rect(roundIt(randint(0,1050),50),roundIt(randint(0,450),50),100,100)
+        if not robotHitbox.colliderect(no_spawn_region) and robotHitbox.collidelist(robot_hitboxes)==-1:
+            robot_hitboxes.append(robotHitbox)
+            break
+
+    for hitbox in robot_hitboxes:
+        objects.append(hitbox)
+    #Rect(randint(0,975),randint(0,405),175,20)
+    #if nospawnregion.colliderect(robot_platforms[0]):
+        #robot_platforms[0]=Rect(randint(0,975),randint(0,405),175,20)
+
+# def shoot():
+#     ang=atan2(p[1]-robot_platforms[0][1],p[0]-robot_platforms[0][0])
+#     vx=cos(ang)*SPEED
+#     vy=sin(ang)*SPEED
+#     bullets.append([robot_platforms[0][0],robot_platforms[0][1],vx,vy])
+    
+# def shooterRobot(start):
+#     current=time.get_ticks()
+#     if current-start>=3000:
+#         shoot()
+#         start=current
+#     for b in bullets[:]:
+#         b[0]+=b[2]
+#         b[1]+=b[3]
+#         if b[0]>1150 or b[0]<0 or b[1]<0 or b[1]>697:
+#             bullets.remove(b)
+#     return start
 
 def drawScene():
     screen.fill(GREY)
-    draw.rect(screen,RED,nospawnregion,1)
+    draw.rect(screen,RED,no_spawn_region,1)
     draw.rect(screen,RED,p,1)
-    for plat in robot_platforms:
-        draw.rect(screen,RED,plat,1)
+    for hitbox in robot_hitboxes:
+        draw.rect(screen,RED,hitbox,1)
     for plat in rect_list:
         screen.blit(block,plat)
+    for b in bullets:
+        draw.circle(screen,GREEN,(int(b[0]),int(b[1])),4)
     row=p_list[0]
     col=int(p_list[1])
-    pic=pics[row][col]
+    pic=player_pics[row][col]
     screen.blit(pic,p)
 
 def hitWalls(x,y,walls):
@@ -95,9 +139,10 @@ def movePlayer(p,move_list):
         if v[Y]==0:
             p_list[0]=2
             move_list[img_speed]=0.15
+    if p[Y]+p[H]==v[2] and v[Y]==0:
+        move_list[dJump]="first jump"
     if keys[K_w] and p[Y]+p[H]==v[2] and v[Y]==0:
         v[Y]=jumpPower
-        move_list[dJump]="first jump"
     if not keys[K_w] and move_list[dJump]=="first jump":
         move_list[dJump]="double jump available"
     if move_list[dJump]=="double jump available" and keys[K_w]:
@@ -118,22 +163,27 @@ def movePlayer(p,move_list):
         else:
             p_list[0]=7
 
-    p_list[1]=(p_list[1]+move_list[img_speed])%len(pics[p_list[0]])
+    p_list[1]=(p_list[1]+move_list[img_speed])%len(player_pics[p_list[0]])
 
     p[X]+=v[X]
     if v[Y]<=100:
         v[Y]+=gravity
+    
+    #phasing through the sides
+    if p[0]+p[W]<0:
+        p[0]=1150+p[0]+p[W]
+    elif p[0]>1150:
+        p[0]=0-p[W]+p[0]-1150
 
 def check(p):
-    global timer
+    current=time.get_ticks()
     for plat in rect_list:
         if p[X]+p[W]>plat[X] and p[X]<plat[X]+plat[W] and p[Y]+p[H]<=plat[Y] and p[Y]+p[H]+v[Y]>=plat[Y]:
             v[Y]=0
             v[2]=plat[Y]
             p[Y]=plat[Y]-p[H]
     if not moved:
-        if timer<180:
-            timer+=1
+        if (current-begin)/1000<3:
             v[Y]=0
         else:
             p[Y]+=int(v[Y])
@@ -143,12 +193,6 @@ def check(p):
 for img in lavaImgs:
     img.set_colorkey(BLACK)
 
-def roundIt(num,round_num):
-    n=0
-    for i in range(0,num+1,round_num):
-        if i+round_num>num:
-            n=i
-    return n
 
 def gaps(x1,y1,x2,y2,map,action):
     gap=[]
@@ -179,20 +223,23 @@ def gaps(x1,y1,x2,y2,map,action):
                 rect_list.remove(gaperaseRect)
         return map
 
-def cleanMap(map):
-    for block in map:
+def cleanMap(new_blocks_drawn,map):
+    for block in new_blocks_drawn:
         if block.collidelist(objects)!=-1:
             map.remove(block)
 
 def drawMap(x1,y1,x2,y2,map):
+    new_blocks_drawn=[]
     if abs(x1-x2)>50 or abs(y1-y2)>50:
         for block in gaps(x1,y1,x2,y2,map,"fill"):
             map.append(block)
+            new_blocks_drawn.append(block)
     else:
         drawRect=Rect(x2,y2,50,50)
         if drawRect not in map:
             map.append(drawRect)
-    cleanMap(map)
+            new_blocks_drawn.append(drawRect)
+    cleanMap(new_blocks_drawn,map)
 
 def eraseMap(x1,y1,x2,y2,map):
     if abs(x1-x2)>50 or abs(y1-y2)>50:
@@ -206,9 +253,12 @@ def animate(lst,counter,speed,x,y):
     screen.blit(lst[int(counter)],(x,y))
     return (counter+speed)%len(lst)
 
+generatePlatform()
+generatePlatform()
+generatePlatform()
+generatePlatform()
+generatePlatform()
 while running:
-    if nospawnregion.colliderect(robot_platforms[0]):
-        robot_platforms[0]=Rect(randint(0,975),randint(0,405),175,20)
     for evt in event.get():
         if evt.type==QUIT:
             running=False
@@ -231,7 +281,9 @@ while running:
     lava_counter=animate(lavaImgs,lava_counter,0.1,0,580)
     movePlayer(p,move_list)
     check(p)
-    print(len(rect_list))
+    #start_timer=shooterRobot(start_timer)
+    print(bullets)
+    #print(len(rect_list))
     myClock.tick(60)
     display.update()
     omx,omy=mx,my
